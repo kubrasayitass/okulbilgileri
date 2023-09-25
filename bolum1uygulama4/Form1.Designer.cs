67f091dcdfef3cd198f30aee57ae12973ebce6fb
kubrasayitass <145451189+kubrasayitass@users.noreply.github.com>
2023-09-25 15:47:57 +0300

    Add files via upload

---
+++ bolum1uygulama4/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace bolum1uygulama4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOkulno = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSınıf = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAd = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btncinsiyet = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnsoyad = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btntelefon = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnÖgrenciad = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnArama = new System.Windows.Forms.Label();
+            this.btnİşlem = new System.Windows.Forms.Label();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnGüncelle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOkulno
+            // 
+            this.btnOkulno.Location = new System.Drawing.Point(91, 27);
+            this.btnOkulno.Name = "btnOkulno";
+            this.btnOkulno.Size = new System.Drawing.Size(99, 27);
+            this.btnOkulno.TabIndex = 0;
+            this.btnOkulno.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Okul no:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnSınıf
+            // 
+            this.btnSınıf.Location = new System.Drawing.Point(91, 60);
+            this.btnSınıf.Name = "btnSınıf";
+            this.btnSınıf.Size = new System.Drawing.Size(99, 28);
+            this.btnSınıf.TabIndex = 2;
+            this.btnSınıf.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sınıf:";
+            // 
+            // btnAd
+            // 
+            this.btnAd.Location = new System.Drawing.Point(328, 27);
+            this.btnAd.Name = "btnAd";
+            this.btnAd.Size = new System.Drawing.Size(103, 27);
+            this.btnAd.TabIndex = 4;
+            this.btnAd.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(299, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ad:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btncinsiyet
+            // 
+            this.btncinsiyet.Location = new System.Drawing.Point(328, 60);
+            this.btncinsiyet.Name = "btncinsiyet";
+            this.btncinsiyet.Size = new System.Drawing.Size(103, 28);
+            this.btncinsiyet.TabIndex = 6;
+            this.btncinsiyet.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(277, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "cinsiyet:";
+            // 
+            // btnsoyad
+            // 
+            this.btnsoyad.Location = new System.Drawing.Point(545, 29);
+            this.btnsoyad.Name = "btnsoyad";
+            this.btnsoyad.Size = new System.Drawing.Size(107, 33);
+            this.btnsoyad.TabIndex = 8;
+            this.btnsoyad.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(499, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Soyad:";
+            // 
+            // btntelefon
+            // 
+            this.btntelefon.Location = new System.Drawing.Point(545, 75);
+            this.btntelefon.Name = "btntelefon";
+            this.btntelefon.Size = new System.Drawing.Size(107, 34);
+            this.btntelefon.TabIndex = 10;
+            this.btntelefon.UseVisualStyleBackColor = true;
+            this.btntelefon.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(499, 96);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Telefon:";
+            // 
+            // btnÖgrenciad
+            // 
+            this.btnÖgrenciad.Location = new System.Drawing.Point(115, 288);
+            this.btnÖgrenciad.Name = "btnÖgrenciad";
+            this.btnÖgrenciad.Size = new System.Drawing.Size(158, 32);
+            this.btnÖgrenciad.TabIndex = 12;
+            this.btnÖgrenciad.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(-1, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Bilgi Girişi";
+            // 
+            // btnArama
+            // 
+            this.btnArama.AutoSize = true;
+            this.btnArama.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnArama.Location = new System.Drawing.Point(-1, 243);
+            this.btnArama.Name = "btnArama";
+            this.btnArama.Size = new System.Drawing.Size(47, 16);
+            this.btnArama.TabIndex = 14;
+            this.btnArama.Text = "Arama";
+            // 
+            // btnİşlem
+            // 
+            this.btnİşlem.AutoSize = true;
+            this.btnİşlem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnİşlem.Location = new System.Drawing.Point(366, 243);
+            this.btnİşlem.Name = "btnİşlem";
+            this.btnİşlem.Size = new System.Drawing.Size(54, 16);
+            this.btnİşlem.TabIndex = 15;
+            this.btnİşlem.Text = "işlemler";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(445, 288);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(100, 49);
+            this.btnKaydet.TabIndex = 16;
+            this.btnKaydet.Text = "kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(569, 288);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(96, 47);
+            this.btnSil.TabIndex = 17;
+            this.btnSil.Text = "sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(40, 307);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Öğrenci Adı:";
+            // 
+            // btnGüncelle
+            // 
+            this.btnGüncelle.Location = new System.Drawing.Point(684, 288);
+            this.btnGüncelle.Name = "btnGüncelle";
+            this.btnGüncelle.Size = new System.Drawing.Size(89, 47);
+            this.btnGüncelle.TabIndex = 19;
+            this.btnGüncelle.Text = "Güncelle";
+            this.btnGüncelle.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-13, 341);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(818, 163);
+            this.dataGridView1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGüncelle);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.btnİşlem);
+            this.Controls.Add(this.btnArama);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnÖgrenciad);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btntelefon);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnsoyad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btncinsiyet);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnAd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSınıf);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOkulno);
+            this.Name = "Form1";
+            this.Text = ",";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOkulno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSınıf;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btncinsiyet;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnsoyad;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btntelefon;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnÖgrenciad;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label btnArama;
+        private System.Windows.Forms.Label btnİşlem;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnGüncelle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
